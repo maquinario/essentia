3b40ca9ecaca7e95a1222c40eeaa20443e4b0cbf
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-06 11:59:13 -0300

    feat: add header

---
+++ wp-content/themes/essentia/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?></title>
+  <link id="page_favicon" href="<?php bloginfo('template_url')?>/favicon.png" rel="icon" type="image/x-icon" />
+  <link rel="stylesheet" href="https://use.typekit.net/kav0jnw.css">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <?php wp_head(); ?>
+</head>
+<body>
