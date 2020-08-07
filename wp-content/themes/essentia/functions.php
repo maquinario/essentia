8c66e72996620a3eb1a5f2feb14606a3f42e7fc6
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-07 11:41:35 -0300

    feat: create basic functions file

---
+++ wp-content/themes/essentia/functions.php
@@ -0,0 +1,63 @@
+<?php
+// Adds post thumbnail support
+add_theme_support('post-thumbnails');
+
+// Custom excerpt mysql_fetch_lengths
+function excerptLength($length) {
+    return 10;
+}
+add_filter('excerpt_length', 'excerptLength');
+
+// Add excerpt to pages
+add_post_type_support( 'page', 'excerpt' );
+add_post_type_support( 'products', 'excerpt' );
+
+// Add order to posts
+add_action('admin_init', 'posts_order');
+function posts_order(){
+  add_post_type_support( 'post', 'page-attributes' );
+}
+
+// Header and footer menus registration
+function register_my_menus() {
+  register_nav_menus( array(
+    'footer_menu' => 'Footer Menu',
+    'navigation' => 'Navigation'
+  ) );
+}
+add_action( 'init', 'register_my_menus' );
+
+// Hide admin bar
+show_admin_bar(false);
+
+// Allow SVG upload
+function upload_svg_files($allowed) {
+    if (!current_user_can('manage_options'))
+      return $allowed;
+    $allowed['svg'] = 'image/svg+xml';
+    return $allowed;
+}
+add_filter('upload_mimes','upload_svg_files');
+// Custom post types
+
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type('projetos',
+    array(
+      'labels' => array(
+        'name' => __('Projetos'),
+        'singular_name' => __('Projeto')
+      ),
+      'public' => true,
+      'menu_icon' => 'dashicons-location',
+      'hierarchical' => true,
+      'has_archive' => true,
+      'taxonomies' => array('category'),
+      'supports' => array(
+            'title',
+            'editor',
+            'thumbnail')
+    )
+  );
+}
