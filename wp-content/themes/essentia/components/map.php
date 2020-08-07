9bb65d2f141578648d89db9f5422c2cce0866471
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-07 18:24:39 -0300

    feat: create map structure

---
+++ wp-content/themes/essentia/components/map.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__.'/../helpers/CategoryParse.php';
+
+query_posts('post_type=projetos');
+if(have_posts()): ?>
+<section class="map">
+  <div class="container">
+    <div class="map-wrapper">
+      <div class="map-projects">
+        <ul class="map-list">
+          <?php while(have_posts()):the_post();
+          $category_parse = new CategoryParse(get_the_category()); ?>
+            <li
+              data-coords="<?php the_field('coordinates'); ?>"
+              class="map-item <?php echo $category_parse->createClassFromCategories(); ?>">
+              <h3 class="map-itemTitle"><?php the_title(); ?></h3>
+              <div class="map-itemLocation">
+                <?php the_field('location'); ?>
+              </div>
+            </li>
+          <?php endwhile; ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<?php endif; ?>
