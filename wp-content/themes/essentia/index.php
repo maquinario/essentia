864d0c440d08a14bca1203bf5e0dda6abe60c0ae
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-07 16:30:41 -0300

    feat: add banner in home page

--- wp-content/themes/essentia/index.php
+++ wp-content/themes/essentia/index.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
 
+<?php query_posts('pagename=home'); 
+if(have_posts()):while(have_posts()):the_post(); ?>
+
+<?php $banner = get_field('banner'); ?>
+<section class="bannerHome">
+  <figure class="bannerHome-image">
+    <?php $image = $banner['image'];
+    if($image): ?><img src="<?php echo $image['url']  ?>" alt="<?php echo $image['title'] ?>"><?php endif; ?>
+  </figure>
+  <div class="bannerHome-content">
+    <h2 class="bannerHome-title"><?php echo $banner['title']; ?></h2>
+  </div>
+</section>
+
+<?php endwhile; endif; ?>
+
+
 <?php get_footer() ?>
