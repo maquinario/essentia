5dfa5c840cd1f950f6bb6e11a2ec69ea842c5251
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-07 11:41:18 -0300

    chore: add analytics

--- wp-content/themes/essentia/footer.php
+++ wp-content/themes/essentia/footer.php
@@ -1,5 +1,12 @@
 
-
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-30834803-39"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-30834803-39');
+</script>
 <?php wp_footer(); ?>  
 </body>
 </html>
