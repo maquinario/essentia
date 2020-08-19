bc0988f13c46f2be776b2e6bb592f038774cb5ba
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-19 16:18:29 -0300

    fix: missing semicolon

--- wp-content/themes/essentia/header.php
+++ wp-content/themes/essentia/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
-  <link id="page_favicon" href="<?php bloginfo('template_url')?>/favicon.png" rel="icon" type="image/x-icon" />
+  <link id="page_favicon" href="<?php bloginfo('template_url'); ?>/favicon.png" rel="icon" type="image/x-icon" />
   <link rel="stylesheet" href="https://use.typekit.net/kav0jnw.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <?php wp_head(); ?>
