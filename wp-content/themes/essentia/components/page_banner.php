40bcc94551c6356452271bdad61a15c1f9914bab
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-08 15:43:27 -0300

    feat: add pageBanner structure

---
+++ wp-content/themes/essentia/components/page_banner.php
@@ -0,0 +1,17 @@
+<?php $banner = get_field('banner'); 
+if($banner): ?>
+<section class="pageBanner">
+  <div class="container">
+    <div class="pageBanner-content">
+      <h2 class="pageBanner-title"><?= $banner['title']; ?></h2>
+      <div class="pageBanner-text">
+        <?= $banner['text']; ?>
+      </div>
+    </div>
+    <figure class="pageBanner-image">
+      <?php $image = $banner['image']; ?>
+      <img src="<?= $image['url']; ?>" alt="<?= $image['title']; ?>">
+    </figure>
+  </div>
+</section>
+<?php endif; ?>
---
+++ wp-content/themes/essentia/page-institucional.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+
+
+<?php get_template_part('components/page_banner'); ?>
+
+<?php get_template_part('components/contact_form'); ?>
+<?php get_footer(); ?>
