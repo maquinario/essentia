bd207b14c05fc07e91f05c8f6fefd925b35b2bd4
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-14 17:21:40 -0300

    feat: add navigation menu

--- wp-content/themes/essentia/header.php
+++ wp-content/themes/essentia/header.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
-  <link id="page_favicon" href="<?php bloginfo('template_url'); ?>/favicon.png" rel="icon" type="image/x-icon" />
+  <link id="page_favicon" href="<?php bloginfo(
+    'template_url'
+  ); ?>/favicon.png" rel="icon" type="image/x-icon" />
   <link rel="stylesheet" href="https://use.typekit.net/kav0jnw.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <?php wp_head(); ?>
@@ -13,10 +15,17 @@
 
 <header>
   <h1 class="logo">
-    <a href="<?= site_url(); ?>"><?php bloginfo('name') ?></a>
+    <a href="<?= site_url() ?>"><?php bloginfo('name'); ?></a>
   </h1>
+  <nav class="navigation">
+    <?php wp_nav_menu(['menu' => 'navigation']); ?>
+  </nav>
   <nav class="social">
-    <?php $linkedin = get_field('linkedin', 'option'); 
-    if($linkedin): ?><a href="<?= $linkedin; ?>" target="_blank" class="icon-linkedin" rel="noreferrer noopener">Linkedin</a><?php endif; ?>
+    <?php
+    $linkedin = get_field('linkedin', 'option');
+    if (
+      $linkedin
+    ): ?><a href="<?= $linkedin ?>" target="_blank" class="icon-linkedin" rel="noreferrer noopener">Linkedin</a><?php endif;
+    ?>
   </nav>
 </header>
