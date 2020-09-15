6369dc45e67671dc3dcae517bd419943c3c56628
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-15 15:30:49 -0300

    fix: adjust general bug fixes

--- wp-content/themes/essentia/components/page_banner.php
+++ wp-content/themes/essentia/components/page_banner.php
@@ -1,16 +1,16 @@
-<?php $banner = get_field('banner'); 
-if($banner): ?>
+<?php $banner = get_field('banner', $post->ID);
+if ($banner): ?>
 <section class="pageBanner">
   <div class="container">
     <div class="pageBanner-content">
-      <h2 class="pageBanner-title"><?= $banner['title']; ?></h2>
+      <h2 class="pageBanner-title"><?= $banner['title'] ?></h2>
       <div class="pageBanner-text">
-        <?= $banner['text']; ?>
+        <?= $banner['text'] ?>
       </div>
     </div>
     <figure class="pageBanner-image">
       <?php $image = $banner['image']; ?>
-      <img src="<?= $image['url']; ?>" alt="<?= $image['title']; ?>">
+      <img src="<?= $image['url'] ?>" alt="<?= $image['title'] ?>">
     </figure>
   </div>
 </section>
